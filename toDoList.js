76b35d6dfb476416347f4c5fa2e8aff08d807a3f
lere dakshak mamman <dshak0672@gmail.com>
2025-04-05 12:21:05 +0000

    add toDos function iniated

--- toDoList.js
+++ toDoList.js
@@ -1,1 +1,24 @@
+// Todo List App
 
+// Goal: Simple to-do app with in-memory task storage.
+// Each task should have an: id, title, completed
+
+// Features:
+
+// Add task
+// Mark task as done/undone
+// Filter task according to:  completed | pending tasks
+// Delete task
+
+function addTask(title, id) {
+    let tasks = []; 
+    let newTask = {     
+    title: title,  
+    id: id,         
+    completed: false   
+  };
+  tasks.push(newTask);
+  console.log("Task added:", newTask);
+}
+
+addTask("Prayer", 1);
