dda96e295ef7dfdfb28cd69f33147e551ec597f6
lere dakshak mamman <dshak0672@gmail.com>
2025-04-05 21:55:59 +0000

    mark as done function implemmented

--- toDoList.js
+++ toDoList.js
@@ -10,8 +10,9 @@
 // Filter task according to:  completed | pending tasks
 // Delete task
 
-function addTask(title, id) {
-    let tasks = []; 
+let tasks = []; 
+
+function addToDos(title, id) {
     let newTask = {     
     title: title,  
     id: id,         
@@ -20,5 +21,19 @@ function addTask(title, id) {
   tasks.push(newTask);
   console.log("Task added:", newTask);
 }
+addToDos("Prayer", 1);
+addToDos("Study", 2);
+addToDos("Exercise", 3);
+addToDos("Read", 4);
 
-addTask("Prayer", 1);
+function markAsDone(id) {
+  tasks.forEach((task) => {
+    if (task.id === id) {
+      task.completed = true;
+      console.log("Task marked as done:", task);
+    }
+  });
+}
+markAsDone(1);
+markAsDone(4);
+
